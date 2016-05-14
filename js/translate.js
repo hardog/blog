46a68f12563010a8cc1bc1a930b595fee8f3888a
Frand <peng.zhao@husor.com.cn>
2016-05-14 22:42:41 +0800

    Site updated: 2016-05-14 22:42:40

--- js/translate.js
+++ js/translate.js
@@ -1,1 +1,25 @@
-console.log('hello this is translate');
+;(function($){
+	function translateTozhCN(eng){
+		var url = 'http://fanyi.youdao.com/openapi.do? \
+			  keyfrom=startexample \
+			  &key=1172896456 \
+			  &type=data \
+			  &doctype=json \
+			  &version=1.1 \
+			  &q=' + eng;
+
+		$.get(url, function(data, status, xhr){
+			console.log('data', data);
+		});
+	}
+
+	function showSelect(){
+		var selecter = window.getSelection();
+
+        if(selecter != null && $.trim(selecter) != ''){
+        	translateTozhCN(selecter);
+    	}
+	}
+
+	document.onmouseup = showSelect;
+})(Zepto)
