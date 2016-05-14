a1585b7cb30fb997c0c3cfe10d00c81fcd48cde8
Frand <peng.zhao@husor.com.cn>
2016-05-14 22:56:32 +0800

    Site updated: 2016-05-14 22:56:32

--- js/translate.js
+++ js/translate.js
@@ -1,12 +1,15 @@
 ;(function($){
 	function translateTozhCN(eng){
-		var url = 'http://fanyi.youdao.com/openapi.do? \
-			  keyfrom=startexample \
-			  &key=1172896456 \
-			  &type=data \
-			  &doctype=json \
-			  &version=1.1 \
-			  &q=' + eng;
+		var param = $.param({
+			keyfrom: 'startexample',
+			key: 1172896456,
+			type: 'data',
+			doctype: 'json',
+			version: '1.1',
+			q: eng
+		});
+
+		var url = 'http://fanyi.youdao.com/openapi.do?' + param;
 
 		$.get(url, function(data, status, xhr){
 			console.log('data', data);
