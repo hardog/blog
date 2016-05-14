1367d0d9e1111a9ac124752d11a4c1431ba8c89e
Frand <peng.zhao@husor.com.cn>
2016-05-15 00:20:29 +0800

    Site updated: 2016-05-15 00:20:28

--- js/translate.js
+++ js/translate.js
@@ -1,30 +1,56 @@
-;(function($){
-	function translateTozhCN(eng){
-		var param = $.param({
-			keyfrom: 'startexample',
-			key: '1172896456',
-			type: 'data',
-			doctype: 'json',
-			version: '1.1',
-			translate: 'on',
-			only: 'translate',
-			q: eng
-		});
+// global var
+var curPosEvent;
 
-		var url = 'http://fanyi.youdao.com/openapi.do?' + param;
-		$.get(url, function(data, status, xhr){
-			console.log('data', data);
-		});
+function show(data){
+	// clear before
+	$('.showText').remove();
+	if(data.errorCode !== 0){return;}
+	var width  = curPosEvent.clientX;
+	var height = curPosEvent.clientY - 40;
+
+	console.log(curPosEvent.clientX, curPosEvent.clientY);
+	var style = 'left:' + width + 'px;top:' + height + 'px';
+	$('body').append('<div class="showText" style="' + style + '">' 
+					 + data.translation.join(',')
+					 + '</div>');
+
+	// 500ms clear showText
+	setTimeout(function(){
+		$('.showText').remove();
+	}, 1000);
+	// alert(data.translation.join(','));
+}
+
+function translateTozhCN(eng){
+	var param = $.param({
+		keyfrom: 'startexample',
+		key: '1172896456',
+		type: 'data',
+		doctype: 'jsonp',
+		version: '1.1',
+		callback: 'show',
+		only: 'translate',
+		q: eng
+	});
+
+	var url = 'http://fanyi.youdao.com/openapi.do?' + param;
+	$('body').append('<script src=' + url + '></script>')
+}
+
+function showSelect(){
+	var sel = window.getSelection();
+	var selstr = (sel || '').toString();
+
+    if($.trim(selstr) != ''){
+    	translateTozhCN(selstr);
 	}
+}
 
-	function showSelect(){
-		var sel = window.getSelection();
-		var selstr = (sel || '').toString();
+$(function(){
+	$(document).on('mouseup', function(e){
+		curPosEvent = e;
+		showSelect();
+	});
+});
 
-        if($.trim(selstr) != ''){
-        	translateTozhCN(selstr);
-    	}
-	}
-
-	document.onmouseup = showSelect;
-})(Zepto)
+document.onmouseup = showSelect;
