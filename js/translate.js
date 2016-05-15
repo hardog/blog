4865ea797c2b4034d6527eccca20336122ed313a
Frand <peng.zhao@husor.com.cn>
2016-05-15 15:35:28 +0800

    Site updated: 2016-05-15 15:35:27

--- js/translate.js
+++ js/translate.js
@@ -1,23 +1,12 @@
-// global var
-var curPosEvent;
-
 function show(data){
 	// clear before
-	$('.showText').remove();
 	if(data.errorCode !== 0){return;}
-	var width  = curPosEvent.clientX;
-	var height = curPosEvent.clientY - 40;
-
-	var style = 'left:' + width + 'px;top:' + height + 'px';
-	$('body').append('<div class="showText" style="' + style + '">' 
-					 + data.translation.join(',')
-					 + '</div>');
-
+	$('.showText').html(data.translation.join(','));
+	$('.showText').css('display', 'block');
 	// 500ms clear showText
 	setTimeout(function(){
-		$('.showText').remove();
+		$('.showText').css('display', 'none');
 	}, 1000);
-	// alert(data.translation.join(','));
 }
 
 function translateTozhCN(eng){
@@ -46,17 +35,5 @@ function showSelect(){
 }
 
 $(function(){
-	$(document).on('mouseup', function(e){
-		curPosEvent = e;
-		showSelect();
-	});
-
-	var agent = navigator.userAgent;
-	if(agent.match(/(iPhone|iPod|Android|ios)/i)){
-		console.log('is mobile from touch');
-		$(document).on('touchend', function(e){
-			curPosEvent = e;
-			showSelect();
-		});
-	}
+	$('.tozhCN').on('click', showSelect);
 });
